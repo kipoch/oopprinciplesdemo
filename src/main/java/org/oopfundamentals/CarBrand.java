e272a2602a2f1c772e2ce60bb5b4ee9cac04453f
DamaluMalisa <damalumalisa@gmail.com>
2024-05-06 16:01:32 +0300

    second commit

---
+++ src/main/java/org/oopfundamentals/CarBrand.java
@@ -0,0 +1,28 @@
+package org.oopfundamentals;
+
+public class CarBrand {
+    private String name;
+    private String country;
+
+    public CarBrand(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
